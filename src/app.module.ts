2acb664464f2bf1ee69667d6d11864d097a1a536
majudlorenzoni <juliamaria892@gmail.com>
2024-08-31 17:31:20 -0300

    fix: configuracao bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,23 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Cliente } from "./domain/entity/cliente.entity";
-import { Servico } from "./domain/entity/servico.entity";
-import { Servidora } from "./domain/entity/servidora.entity";
-import { ServicoTipo } from "./domain/entity/servico-tipo.entity";
-import { TipoServico } from "./domain/entity/tipo-servico.entity";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config(); // Se você estiver usando um arquivo .env
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      database: 'woman-services',
-      username: 'reprograma8',
-      password: 'repro',
-      entities: [Cliente, Servico, Servidora, ServicoTipo, TipoServico],
-      synchronize: true,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // Utilize apenas para desenvolvimento
     }),
+    // Outros módulos
   ],
+  // Controladores e provedores
 })
-
 export class AppModule {}
