27b7e1e898bf18f72767d89f90a3e3341db474e8
majudlorenzoni <juliamaria892@gmail.com>
2024-08-31 16:26:42 -0300

    fix: ajustando configuracoes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // Carrega as variáveis do arquivo .env
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuração básica do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Women Services API')
+    .setDescription('API para contratação de serviços por mulheres')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
