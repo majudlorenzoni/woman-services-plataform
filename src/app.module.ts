d6a95eda13d8203ba05df5100d4f6494160d84b3
majudlorenzoni <juliamaria892@gmail.com>
2024-08-31 17:55:31 -0300

    fix: conexão deploy e bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'dotenv';
 
-config(); // Se você estiver usando um arquivo .env
+config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      synchronize: true, // Utilize apenas para desenvolvimento
+      synchronize: true,
     }),
-    // Outros módulos
   ],
-  // Controladores e provedores
 })
 export class AppModule {}
