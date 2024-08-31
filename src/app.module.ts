ae2283a427a63b52d70bacd0b44a5c0df5654abd
majudlorenzoni <juliamaria892@gmail.com>
2024-08-31 17:15:02 -0300

    fix: conecao banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,24 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import * as dotenv from 'dotenv';
-
-dotenv.config(); 
+import { Cliente } from "./domain/entity/cliente.entity";
+import { Servico } from "./domain/entity/servico.entity";
+import { Servidora } from "./domain/entity/servidora.entity";
+import { ServicoTipo } from "./domain/entity/servico-tipo.entity";
+import { TipoServico } from "./domain/entity/tipo-servico.entity";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+      host: 'localhost',
+      port: 5432,
+      database: 'woman-services',
+      username: 'reprograma8',
+      password: 'repro',
+      entities: [Cliente, Servico, Servidora, ServicoTipo, TipoServico],
+      synchronize: true,
     }),
   ],
 })
+
 export class AppModule {}
