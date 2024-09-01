56dd34b8a76fce3ae2598dda976ac8accda07f57
majudlorenzoni <juliamaria892@gmail.com>
2024-08-31 23:59:31 -0300

    fix: endereco da api

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,22 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('API')
-    .setDescription('The API description')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // Verifica se o ambiente é de desenvolvimento
+  const isDev = process.env.NODE_ENV !== 'production';
 
-  await app.listen(3000);
+  if (isDev) {
+    // Configuração do Swagger apenas em ambientes de desenvolvimento
+    const config = new DocumentBuilder()
+      .setTitle('API')
+      .setDescription('The API description')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
+  // Inicia o servidor na porta especificada ou na porta 3000 por padrão
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
